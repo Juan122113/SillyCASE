7a631f90769527d694cc343e4c97bb778be2c71c
Juan122113 <ammiel89@hotmail.com>
2025-06-25 16:44:44 -0300

    Variable creations

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 function sillycase(silly) {
   
+    let sillyLength = silly.length;
+
+    let sillyLengthHalftRound = Math.ceil(sillyLength / 2);
+
+    let sillyDivided = silly.split(/(\w)/).filter(letter => letter.match(/(\w)/));
+
+    let firstHalft;
+
+    console.log(typeof (sillyDivided[0]))
+
+    return sillyDivided;
 }
+
+console.log(sillycase("brian"));
